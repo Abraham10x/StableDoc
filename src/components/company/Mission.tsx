b70c080717f9fd3f0f59597bc4ab583591af7177
Ameh Abraham <amehabraham811@gmail.com>
2023-03-11 17:57:10 +0100

    added all company address

--- src/components/company/Mission.tsx
+++ src/components/company/Mission.tsx
@@ -43,56 +43,73 @@ const Mission: FC = () => {
         <div className="flex flex-col sm:basis-2/4 xl:basis-[42%] mt-10">
           <div className="flex flex-row">
             <Image
-              src="/img/company/phone.svg"
+              src="/img/support/map-icon.svg"
               alt="phone-icon"
               className="mt-0 lg:h-16 lg:w-auto h-11 w-11 sm:w-14 sm:h-14"
               width={60}
               height={60}
             />
             <div className="flex flex-col ml-5">
-              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight text-text-300">
-                24/7 Support
+              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight bg-gradient-600 bg-clip-text text-transparent-active">
+                Global Operations
               </h3>
-              <p className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
-                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
-                adipiscing conse ctetur adipiscing elitdolor{" "}
-              </p>
+              <div className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
+                <ul>
+                  <li>
+                    3379 Peachtree Road NE (Buckhead), Suite 555 Atlanta, GA
+                    30326, United States
+                  </li>
+                  <li>
+                    Office Phone: +1 (770) 464-6467 Fax: +1 (770) 464-6467
+                  </li>
+                  <li>Cell Phone: +1 (470) 336-061</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className="flex flex-row my-8">
             <Image
-              src="/img/company/message.svg"
+              src="/img/support/map-icon.svg"
               alt="phone-icon"
               className="mt-0 lg:h-16 lg:w-auto h-11 w-11 sm:w-14 sm:h-14"
               width={60}
               height={60}
             />
             <div className="flex flex-col ml-5">
-              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight text-text-300">
-                Lorem Ipsum
+              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight bg-gradient-600 bg-clip-text text-transparent-active">
+                Africa Operations
               </h3>
-              <p className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
-                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
-                adipiscing conse ctetur adipiscing elitdolor{" "}
-              </p>
+              <div className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
+                <ul>
+                  <li>4 Olu Agabi Close Life Camp Abuja FCT Nigeria</li>
+                  <li>Cell Phone: Phone: +234 91 6262 7430</li>
+                </ul>
+              </div>
             </div>
           </div>
           <div className="flex flex-row">
             <Image
-              src="/img/company/badge.svg"
+              src="/img/support/map-icon.svg"
               alt="phone-icon"
               className="mt-0 lg:h-16 lg:w-auto h-11 w-11 sm:w-14 sm:h-14"
               width={60}
               height={60}
             />
             <div className="flex flex-col ml-5">
-              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight text-text-300">
-                Overall Guarantee
+              <h3 className="font-bold text-base sm:text-xl lg:text-[26px] sm:leading-tight bg-gradient-600 bg-clip-text text-transparent-active">
+                Asia-Pacific Operations
               </h3>
-              <p className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
-                Lorem ipsum dolor sit amet doloroli sitiol conse ctetur
-                adipiscing conse ctetur adipiscing elitdolor{" "}
-              </p>
+              <div className="font-normal text-[15px] sm:text-lg lg:text-xl text-text-500 mt-3">
+                <ul>
+                  <li>
+                    {" "}
+                    No 11, 6th Pillaiyar Koil Street, Ambal Nagar,
+                    Ekkatthuthangal, Chennai 600 032, India
+                  </li>
+                  <li>Office Phone: +91 222 50312</li>
+                  <li>Mobile Phone: +91 98408 00863</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
